299568689881cc0487597720b40831a7ca518cd9
Resham <Resham.kadel@gmail.com>
2018-01-17 10:41:51 +0700

    Init Project

---
+++ NewsNepal/Pages/NewsTabHome.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NewsNepal.Pages
+{
+    public partial class NewsTabHome : TabbedPage
+    {
+        public NewsTabHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
